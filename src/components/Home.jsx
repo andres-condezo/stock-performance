1419149bd7f7fce6ec8479bc868ceba2690338c9
andres-condezo <ra.condezomonge@gmail.com>
2022-03-12 15:44:16 -0500

    Feat: [Add search bar component]

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { FcStatistics } from 'react-icons/fc';
 import Company from './Company';
@@ -8,6 +8,8 @@ import './Company.scss';
 const Home = () => {
   const companiesList = useSelector((state) => state.homeReducer);
   const dispatch = useDispatch();
+
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     if (companiesList.length === 0) {
@@ -25,18 +27,26 @@ const Home = () => {
           <h1>COMPANY&apos;S STOCK PERFORMANCE</h1>
         </div>
         <p className="company-subtitle">STATS BY COMPANY</p>
+        <input
+          type="text"
+          onChange={({ target }) => { setSearch(target.value); }}
+          placeholder="Search by company"
+          className="input"
+        />
         <section className="company-section">
           {
-            companiesList.map((company) => (
-              <Company
-                key={company.symbol}
-                symbol={company.symbol}
-                name={company.name}
-                change={company.change}
-                price={company.price}
-                changesPercentage={company.changesPercentage}
-              />
-            ))
+            companiesList
+              .filter((company) => company.name.toLowerCase().includes(search.toLowerCase()))
+              .map((company) => (
+                <Company
+                  key={company.symbol}
+                  symbol={company.symbol}
+                  name={company.name}
+                  change={company.change}
+                  price={company.price}
+                  changesPercentage={company.changesPercentage}
+                />
+              ))
           }
         </section>
       </div>
